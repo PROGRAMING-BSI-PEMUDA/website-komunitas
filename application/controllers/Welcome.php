c9d1b7747333df0535e47bbded62db47bdc10163
tedilesmana <tedi.lesmana0123@gmail.com>
2019-08-10 19:36:06 +0700

    membuat bagian templating dasar

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,10 +1,17 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Welcome extends CI_Controller
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library('template');
+	}
 
 	public function index()
 	{
-		$this->load->view('index');
+		$this->template->template('index');
 	}
 }
---
+++ application/views/Template/v_header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+
+	<!-- Icon Logo -->
+	<link rel="../shortcut icon" href="assets/img/logo.png" type="image/x-icon">
+	<!-- Material Design for Bootstrap CSS -->
+	<link rel="stylesheet" href="<?= base_url('assets/bootstrap/css/'); ?>bootstrap.min.css">
+	<link rel="stylesheet" href="<?= base_url('assets/bootstrap/css/'); ?>bootstrap.css">
+	<!-- fontawesome-->
+	<link rel="stylesheet" href="<?= base_url('assets/font-awesome/'); ?>css/font-awesome.css">
+	<!-- Material Design Bootstrap -->
+	<link href="<?= base_url('assets/bootstrap/css/'); ?>mdb.min.css" rel="stylesheet">
+	<!-- Your custom styles (optional) -->
+	<link href="<?= base_url('assets/bootstrap/css/'); ?>style.css" rel="stylesheet">
+
+</head>
