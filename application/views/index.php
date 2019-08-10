55a65743a192ab8c77e220215ecb553445df76da
Rizky Aji Rahmansyah <rizkyajirahmansyah@gmail.com>
2019-08-10 11:09:54 +0700

    Mengganti title info

--- application/views/index.php
+++ application/views/index.php
@@ -92,7 +92,7 @@
 <button type="button" class="btn btn-success">Success</button>
 
 <!-- Contextual button for informational alert messages -->
-<button type="button" class="btn btn-info">Info</button>
+<button type="button" class="btn btn-info">Infos</button>
 
 <!-- Indicates caution should be taken with this action -->
 <button type="button" class="btn btn-warning">Warning</button>
